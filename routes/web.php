b27658c2aba6f787969a21cccc471d366d6bd9a4
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-08-16 13:51:52 -0400

    first

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Events\UserSignedUp;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('using-facade', function () {
+    Event::fire(new UserSignedUp('John', 30));
+});
---
+++ tests/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        Event::shouldReceive('fire')->once();
+
+        $this->visit('/using-facade');
+    }
+}
+
